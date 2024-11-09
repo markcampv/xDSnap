1361c5d8663aadc11070471dc0cbb272dc3c5616
Mark Campbell-Vincent <mark.cv@mark.cv-X96K5442NH>
2024-11-09 18:52:51 -0500

    add top level command

---
+++ cmd/root.go
@@ -0,0 +1,20 @@
+// cmd/root.go
+package main
+
+import (
+	"os"
+	"github.com/markcampv/xDSnap/cmd" // Import the capture command setup
+	"github.com/spf13/pflag"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+)
+
+func main() {
+	flags := pflag.NewFlagSet("kubectl-xdsnap", pflag.ExitOnError)
+	pflag.CommandLine = flags
+
+	root := cmd.NewRootCommand(genericclioptions.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr})
+
+	if err := root.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
