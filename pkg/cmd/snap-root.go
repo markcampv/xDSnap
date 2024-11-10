fa4e36fc81b7b4fc2b7a40457d5a6b24375c0c27
Mark Campbell-Vincent <mark.cv@mark.cv-X96K5442NH>
2024-11-09 23:44:30 -0500

    update directory hierarchy

--- pkg/cmd/snap-root.go
+++ pkg/cmd/snap-root.go
@@ -3,17 +3,19 @@ package cmd
 
 import (
     "github.com/spf13/cobra"
+    "k8s.io/cli-runtime/pkg/genericclioptions"
 )
 
-// NewRootCommand initializes the root command for the CLI
-func NewRootCommand() *cobra.Command {
+// NewRootCommand creates the root command for xDSnap
+func NewRootCommand(streams genericclioptions.IOStreams) *cobra.Command {
     rootCmd := &cobra.Command{
         Use:   "xdsnap",
-        Short: "xDSnap is a CLI tool for capturing Envoy snapshots",
+        Short: "XDSnap captures Envoy state snapshots across Kubernetes pods for troubleshooting.",
     }
 
-    // Add subcommands to the root command
-    rootCmd.AddCommand(NewCaptureCommand()) 
+    // Add the capture subcommand
+    rootCmd.AddCommand(NewCaptureCommand(streams))
 
     return rootCmd
 }
+
